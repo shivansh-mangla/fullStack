8cc743e8675682897e3f644ba2dcfa69b84aaa85
Shivansh <shivanshmangla72@gmail.com>
2024-11-16 14:10:35 +0530

    Added Yup

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,14 +1,35 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import axios from "axios";
 
 
 function CreatePost(){
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: ""
+  };
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/myPosts", data).then((response) => {
+      console.log("It Worked!!");
+    })
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required(),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  })
+
   return(
     <>
     <div className="createPostPage">
-      <Formik>
+      <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
         <Form className="formContainer">
           <label>Title: </label>
+          <ErrorMessage name="title" component='span' />
           <Field
             className="inputCreatePost"
             name="title"
@@ -16,6 +37,7 @@ function CreatePost(){
           />
 
           <label>Post Text: </label>
+          <ErrorMessage name="postText" component='span' />
           <Field
             className="inputCreatePost"
             name="postText"
@@ -23,6 +45,7 @@ function CreatePost(){
           />
 
           <label>Username: </label>
+          <ErrorMessage name="username" component='span' />
           <Field
             className="inputCreatePost"
             name="username"
